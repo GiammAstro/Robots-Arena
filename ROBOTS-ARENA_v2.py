dee5c25a355e8ab2015d46f33efe05d4e66a6b66
Giammarco Cialone <giammarcocialone@gmail.com>
2020-02-24 22:26:34 +0100

    developed second version of ROBOTS-ARENA
    Now the plot of the arena is included in a tkinter canvas and the whole layout of the app should be more easy to manage. Start, Pause and Quit buttons have been implemented and they are fully functional.

--- ROBOTS-ARENA_v2.py
+++ ROBOTS-ARENA_v2.py
@@ -1,3 +1,4 @@
+#from gui import game_gui
 from game_app import game
 import time
 import sys
@@ -11,7 +12,7 @@ game = game(fps, match_length)
 
 #----------------MAIN---------------------
 def main():
-
+    
     #--------MATCH INITIALIZATION---------
     #importing the robots in the robots folder
     game.import_robots()
@@ -22,20 +23,31 @@ def main():
     #drawing the robots in the arena on their initial position
     game.draw_robots()
 
+    #-------USER START INPUT-------------
+    while game.start == False:
+        game.refresh_game()
+
     #--------MATCH COUNTDOWN-------------
     #countdown
     game.display_countdown()
     
     #--------MATCH EVOLUTION-------------
     start_time = time.time()
-    while time.time() - start_time < match_length + 1:
+    delta_time = 0
+    #we loop until the maximum time is not finished or there is a winner
+    while delta_time < match_length + 1 or game.robots_alive_number != 1:
+        if game.pause == True:
+            while game.start != True:
+                time.sleep(0.1)
+                game.refresh_game()
         #starting evolving the match
-        game.animate_match(time.time() - start_time)
+        time_1 = time.time()
+        game.animate_match(delta_time)
         game.check_hit()
         game.draw_robots()
         game.draw_shots()
         time.sleep(1/fps)
         sys.stdout.flush()
-    
+        delta_time += time.time() - time_1
     
 main()
